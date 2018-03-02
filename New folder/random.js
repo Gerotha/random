36836fd7ec73e3be0841524bccd3cc1d9ab82f88
logonrm <logonrm@fiap.com.br>
2018-03-02 11:29:39 -0300

    funcionando mais ou menos

--- New folder/random.js
+++ New folder/random.js
@@ -1,28 +1,30 @@
+var stringola;
+
 function getValues() {
-    var setup = {
-      spreadsheetId: '130zKSslF0Sr4ZCoOWJN_jPeMiVO8R2lzJaS_FNQ01uY',
-      range: 'Página1!A1:B'
-    };
+  var setup = {
+    spreadsheetId: '130zKSslF0Sr4ZCoOWJN_jPeMiVO8R2lzJaS_FNQ01uY',
+    range: 'Página1!A1:B'
+  };
 
-    
-    var request = gapi.client.sheets.spreadsheets.values.get(setup);
-    request.then(function(response) {
-      // TODO: Change code below to process the `response` object:
-      console.log(response.result);
-    }, function(reason) {
-      console.error('error: ' + reason.result.error.message);
-    });
-  }
+  
+  var request = gapi.client.sheets.spreadsheets.values.get(setup);
+  request.then(function(response) {
+    // TODO: Change code below to process the `response` object:
+    stringola = response.result;
+  }, function(reason) {
+    console.error('error: ' + reason.result.error.message);
+  });
+}
 
-  function initClient() {
-    var API_KEY = 'AIzaSyAxzmVZDCncosp4DeFQphi39xRhjoXdcSg';
+function initClient() {
+  var API_KEY = 'AIzaSyAxzmVZDCncosp4DeFQphi39xRhjoXdcSg';
 
-    gapi.client.init({
-      'apiKey': API_KEY,
-      'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    }).then(console.log('Deu bom!'));
-  }
+  gapi.client.init({
+    'apiKey': API_KEY,
+    'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  }).then(console.log('Deu bom!'));
+}
 
-  function handleClientLoad() {
-  gapi.load('client', initClient);
+function handleClientLoad() {
+gapi.load('client', initClient);
 }
